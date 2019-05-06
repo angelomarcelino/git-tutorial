10a392a9e95fa4df5f7e451c493eb33d3e98a5e7
angelomarcelino <amc.marcelino.cordeiro@gmail.com>
2019-05-06 14:58:20 -0300

    change main.py teste

--- python/main.py
+++ python/main.py
@@ -8,4 +8,4 @@ img = img(240, 320, 127)
 img.paintRectangle(30, 10, 180, 160, preto)
 img.paintRectangle(50, 50, 200, 200, branco)
 
-img.salvaImagem("imagemTestePython.pgm")
+img.salvaImagem("imagemTestePythonTeste.pgm")
