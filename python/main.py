a85e2c109302cce4a9bb763813f53396984403d0
angelomarcelino <amc.marcelino.cordeiro@gmail.com>
2019-05-06 14:43:48 -0300

    change main.py

--- python/main.py
+++ python/main.py
@@ -4,16 +4,8 @@ cor = 127
 branco = 255
 preto = 0
 img = img(240, 320, 127)
-x1 = 30
-y1 = 10
-x2 = 180
-y2 = 160
-x11 = 50
-y11 = 50
-x22 = 200
-y22 = 200
 
-img.paintRectangle(x1, y1, x2, y2, preto)
-img.paintRectangle(x11, y11, x22, y22, branco)
+img.paintRectangle(30, 10, 180, 160, preto)
+img.paintRectangle(50, 50, 200, 200, branco)
 
 img.salvaImagem("imagemTestePython.pgm")
